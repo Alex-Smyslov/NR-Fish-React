3fe8a1590a547c2a1bd913a47bba7e618b61603a
Alex <Smyslov.A.S.2017@yandex.ru>
2023-04-18 14:48:37 +0300

    add neon effect

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -3,33 +3,34 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y, EffectCube } from 'swiper';
 import 'swiper/swiper-bundle.min.css';
+import './Slider.module.scss';
 
 function Slider({ slides }) {
-  return (
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y, EffectCube]}
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      effect={'cube'}
-      cubeEffect={{
-        shadow: true,
-        slideShadows: true,
-        shadowOffset: 80,
-        shadowScale: 1,
-      }}
-    >
-      {slides.map((slide) => (
-        <SwiperSlide key={slide.image}>
-          <img src={slide.image} alt={slide.title} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
+	return (
+		<Swiper className='swiper'
+			modules={[Navigation, Pagination, Scrollbar, A11y, EffectCube]}
+			spaceBetween={50}
+			slidesPerView={1}
+			navigation
+			pagination={{ clickable: true }}
+			scrollbar={{ draggable: true }}
+			onSlideChange={() => console.log('slide change')}
+			onSwiper={(swiper) => console.log(swiper)}
+			effect={'cube'}
+			cubeEffect={{
+				shadow: true,
+				slideShadows: true,
+				shadowOffset: 80,
+				shadowScale: 1,
+			}}
+		>
+			{slides.map((slide) => (
+				<SwiperSlide key={slide.image}>
+					<img src={slide.image} alt={slide.title} />
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
 }
 
 export default Slider;
--- src/pages/Galary.tsx
+++ src/pages/Galary.tsx
@@ -6,46 +6,27 @@ import Slider from '../components/slider/Slider';
 import slides from '../components/slider/mock.json';
 
 const Galary = () => {
-  return (
-    <main className="galary section">
-      <div className="container">
-        {/* <SectionTitle text="Галерея" />
-        <section className="galary__main">
-          <div className="galary__main-container container"></div>
-        </section> */}
+	return (
+		<main className="galary section">
+			<div className="container">
+				<section className="galary__examples">
+					<div className="galary__examples-container container">
+						<SectionTitle text="Фотогалерея магазина" />
+						<Slider slides={slides} />
+					</div>
+				</section>
 
-        <section className="galary__examples">
-          <div className="galary__examples-container container">
-            <SectionTitle text="Фотогалерея магазина" />
-            <Slider slides={slides} />
-          </div>
-        </section>
-
-        <section className="galary__section container">
-          <SectionTitle text="Фотографии покупателей" />
-          <div className="gallery__plit">
-            {galaryTile.map((tile) => {
-              return <GalaryTileItem key={tile.id} img={tile.img} alt={tile.alt} />;
-            })}
-          </div>
-
-          <div className="slider2">
-            <div className="slider__container">
-              <button className="slider__btn slider__btn-left">
-                <i className="fas fa-chevron-left"></i>
-              </button>
-              <button className="slider__btn slider__btn-right">
-                <i className="fas fa-chevron-right"></i>
-              </button>
-            </div>
-            <button className="slider__close">
-              <i className="far fa-times-circle"></i>
-            </button>
-          </div>
-        </section>
-      </div>
-    </main>
-  );
+				<section className="galary__section container">
+					<SectionTitle text="Фотографии покупателей" />
+					<div className="gallery__plit">
+						{galaryTile.map((tile) => {
+							return <GalaryTileItem key={tile.id} img={tile.img} alt={tile.alt} />;
+						})}
+					</div>
+				</section>
+			</div>
+		</main>
+	);
 };
 
 export default Galary;
