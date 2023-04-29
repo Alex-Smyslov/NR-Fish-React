84920887205ecc3fb434448a504269a1bb95690f
Alex-Smyslov <112191344+Alex-Smyslov@users.noreply.github.com>
2023-04-29 07:50:45 +0300

    fix .eslintr

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,26 @@ import NotFound from './Containers/NotFound';
 import ScrollToTop from './utils/scrollToTop';
 import Header from './components/header/Header';
 import { routes } from './utils/constants/routes';
-import { Products } from './components/products';
 import { ProductPage } from './Containers/ProductPage';
 import './styles/main.css';
 
 function App() {
-	return (
-		<div className="App">
-			<ScrollToTop />
-			<Header />
-			<Routes>
-				<Route path={routes.HOME} element={<Home />} />
-				<Route path={routes.CATALOG} element={<Catalog />} >
-					<Route path=":id" element={<ProductPage />} />
-				</Route>
-				<Route path={routes.ABOUT} element={<About />} />
-				<Route path={routes.GALARY} element={<Galary />} />
-				<Route path={routes.NOTFOUND} element={<NotFound />} />
-			</Routes>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path={routes.HOME} element={<Home />} />
+        <Route path={routes.CATALOG} element={<Catalog />}>
+          <Route path=":id" element={<ProductPage />} />
+        </Route>
+        <Route path={routes.ABOUT} element={<About />} />
+        <Route path={routes.GALARY} element={<Galary />} />
+        <Route path={routes.NOTFOUND} element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
