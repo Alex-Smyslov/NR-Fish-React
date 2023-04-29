50933f4bae716a7e0a1a25e17cbc47fddb5713ca
Alex-Smyslov <112191344+Alex-Smyslov@users.noreply.github.com>
2023-04-29 09:50:38 +0300

    2

--- server/index.js
+++ server/index.js
@@ -1,34 +1,29 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 const products = require('./static/products.json');
 
-
 app.use(
-	cors({
-		origin: '*',
-	}),
+  cors({
+    origin: '*',
+  }),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
 
 app.get('/catalog', async (req, res) => {
-
-	await res.json(products)
-})
+  await res.json(products);
+});
 
 app.get('/api/images', async (req, res) => {
-
-	await res.json(
-		{
-			id: '1',
-			title: 'Product 1',
-			description: 'Product 1Product 1Product 1Product 1'
-		}
-	)
-})
+  await res.json({
+    id: '1',
+    title: 'Product 1',
+    description: 'Product 1Product 1Product 1Product 1',
+  });
+});
