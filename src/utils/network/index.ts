175ecf33676e81c449a420837bc3b429cb8864a3
Alex-Smyslov <112191344+Alex-Smyslov@users.noreply.github.com>
2023-04-30 13:43:20 +0300

    71212

--- src/utils/network/index.ts
+++ src/utils/network/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const networkInstance = axios.create({
-  baseURL: 'https://nr-fish-server.vercel.app:3001',
+  baseURL: 'https://nr-fish-server.vercel.app',
   timeout: 2000,
 });
