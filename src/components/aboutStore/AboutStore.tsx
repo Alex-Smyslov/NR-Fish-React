07a43e3270c2f945ba829d2312abcd06adbb6212
Alex-Smyslov <112191344+Alex-Smyslov@users.noreply.github.com>
2023-05-05 21:50:39 +0300

    add content

--- src/components/aboutStore/AboutStore.tsx
+++ src/components/aboutStore/AboutStore.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import img from './image/store.jpg';
 import styles from './AboutStore.module.scss';
 
 const AboutStore = () => {
   return (
     <div className={styles.aboutStore}>
       <div className={styles.aboutStore__img}>
-        <img src="" alt="Store" />
+        <img src={img} alt="Store" />
       </div>
       <div className={styles.aboutStore__description}>
-        <p className={styles.aboutStore__title}>Lorem, ipsum dolor.</p>
+        <p className={styles.aboutStore__title}>НИЖЕГОРОДСКИЙ РЫБОЛОВ52</p>
         <p className={styles.aboutStore__text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vel minima explicabo
-          aspernatur a delectus quia asperiores? Esse, reiciendis fuga beatae repudiandae voluptatem
-          laboriosam, recusandae, quaerat dolore id aspernatur quae.Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Ad vel minima explicabo aspernatur a delectus quia
-          asperiores? Esse, reiciendis fuga beatae repudiandae voluptatem laboriosam, recusandae,
-          quaerat dolore id aspernatur quae.
+          Рыболовный мультибрендовый магазин &quot;Нижегородский рыболов52&quot; находится на рынке
+          более 10 лет. Ассортимент магазина соствавляет более 10 000 наименований товаров для
+          рыбалки и активного отдыха. Наши специалисты помогут подобрать товары под нужные условия
+          ловли и наиболее подходящие для Вас. Широкий спектр товаров, представленный в магазине,
+          точно удовлетворит потребности любого, даже самого требовательного рыболова. Все товары,
+          представленные в магазине, являются официальной продукцией именитых брендов и имеют
+          гарантию производителя. Товар закупается напрямую без посредников, что делает товары
+          дешевле, чем у конкурентов. В магазине имеется возможность приобрести товары в рассрочку.
+          Более того, каждому покупателю предоставляется персональная скидка на товары, которая
+          сделает покупку ещё приятней!
         </p>
       </div>
     </div>
