a2b38888536a666200a3a91e45852ea4bec78715
Alex <Smyslov.A.S.2017@yandex.ru>
2023-05-11 09:25:47 +0300

    fix footer

--- src/components/mobileFooter/MobileFooter.tsx
+++ src/components/mobileFooter/MobileFooter.tsx
@@ -1,22 +1,20 @@
 import './mobileFooter.css';
 
 interface TProps {
-  isOpenMenu: boolean;
-  isMobile: boolean;
+	isOpenMenu: boolean;
+	isMobile: boolean;
 }
 
 const MobileFooter = ({ isOpenMenu, isMobile }: TProps) => {
-  return (
-    <div className={`mobile__wrapp ${isOpenMenu && isMobile ? 'isActive' : ''}`}>
-      <a className="phone mobile__phone" href="tel:+79103979890">
-        +7(910) 397 - 98 - 90
-      </a>
-      <br />
-      <a className="mobile__btn modal--open" href="#!">
-        Оставить заявку
-      </a>
-    </div>
-  );
+	return (
+		<div className={`mobile__wrapp ${isOpenMenu && isMobile ? 'isActive' : ''}`}>
+			<a className="phone mobile__phone" href="tel:+79103979890">
+				+7(910) 397 - 98 - 90
+			</a>
+			<br />
+			<p>Ни хвоста, ни чешуи!</p>
+		</div>
+	);
 };
 
 export default MobileFooter;
