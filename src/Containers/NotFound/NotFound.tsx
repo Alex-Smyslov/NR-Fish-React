a55bebb82f10d60855a80443f9f5d375434d17be
Alex <Smyslov.A.S.2017@yandex.ru>
2023-05-12 16:07:40 +0300

    add 404

--- src/Containers/NotFound/NotFound.tsx
+++ src/Containers/NotFound/NotFound.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
 import BtnMain from '../../components/btnMain/BtnMain';
+import error from './image/404.png'
 import styles from './NotFound.module.scss';
 
 const NotFound = () => {
-  return (
-    <main>
-      <section className={styles.notFound}>
-        <div className={styles.container}>
-          <div className={styles.notFound__grid}>
-            <div className={styles.notFound__image}>
-              <img src="assets/images/404.gif" alt="" />
-            </div>
-            <div className={styles.notFound__bot}>
-              <h1>
-                404
-                <br />
-                NOT FOUND
-              </h1>
-              <h2>
-                Эх, сорвалось...
-                <br />
-                Пожалуйста, вернитесь на Главную
-                <br />
-              </h2>
-              <BtnMain to="/" text={'Main Page'} />
-            </div>
-          </div>
-        </div>
-      </section>
-    </main>
-  );
+	return (
+		<main>
+			<section className={styles.notFound}>
+				<div className={styles.notFound__container}>
+					<img src={error} alt="Error" />
+					<BtnMain to="/" text={'Main Page'} />
+				</div>
+			</section>
+		</main>
+	);
 };
 
 export default NotFound;
--- src/components/mobileFooter/MobileFooter.tsx
+++ src/components/mobileFooter/MobileFooter.tsx
@@ -9,7 +9,7 @@ const MobileFooter = ({ isOpenMenu, isMobile }: TProps) => {
 	return (
 		<div className={`mobile__wrapp ${isOpenMenu && isMobile ? 'isActive' : ''}`}>
 			<a className="phone mobile__phone" href="tel:+79103979890">
-				+7(910) 397 - 98 - 90
+				+7 (910) 397 - 98 - 90
 			</a>
 			<br />
 			<p>Ни хвоста, ни чешуи!</p>
