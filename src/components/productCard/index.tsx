466c07bfb12d01b87a87dcb12ae12b9e255d4d3e
Alex <Smyslov.A.S.2017@yandex.ru>
2023-05-15 09:05:25 +0300

    fix prod card

--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -5,30 +5,32 @@ import { routes } from '../../utils/constants/routes';
 import Rating from '../rating/Rating';
 
 interface IProps {
-  id: number;
-  img: string;
-  title: string;
-  description: string;
-  price: number;
-  rating: number;
+	id: number;
+	img: string;
+	title: string;
+	description: string;
+	price: number;
+	rating: number;
 }
 
 export const ProductCard = ({ id, img, title, price, rating }: IProps) => {
-  return (
-    <>
-      <div className={styles.card}>
-        <img className={styles.card__img} src={img} alt={title} />
-        <NavLink className={styles.card__link} to={`${routes.CATALOG}/${id}`}>
-          <span className={styles.card__label}>{title}</span>
-        </NavLink>
-        <Rating rating={rating} />
-        <p className={styles.card__rating}>
-          Рейтинг: <span>{rating}</span>
-        </p>
-        <p className={styles.card__price}>
-          Цена: <span>{price}</span> руб.
-        </p>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={styles.card}>
+				<img className={styles.card__img} src={img} alt={title} />
+				<div className={styles.card__info}>
+					<NavLink className={styles.card__link} to={`${routes.CATALOG}/${id}`}>
+						<span className={styles.card__label}>{title}</span>
+					</NavLink>
+					<div className={styles.card__rating} >
+						<Rating rating={rating} />
+						<span className={styles.card__rating_number} >{rating}</span>
+					</div>
+					<p className={styles.card__price}>
+						Цена: <span>{price}</span> руб.
+					</p>
+				</div>
+			</div >
+		</>
+	);
 };
